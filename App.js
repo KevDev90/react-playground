539b0af95d24f4e471e474134242d4f0d48b8e2b
Kevin Johnson <kevinj902302@gmail.com>
2021-01-14 11:43:00 -0700

    Other file changes to solve accordion challenge

--- App.js
+++ App.js
@@ -4,37 +4,36 @@ import './App.css';
 import Tooltip from './composition/Tooltip';
 import TheDate from './state/TheDate';
 import Counter from './state/Counter';
+import Tabs from './Tabs'
+import Accordion from './state-drills/Accordion';
 // make 2 tooltips here and another inside the App directly
-const firstTooltip = (
-  <Tooltip color='hotpink' message='tooltip message'>
-   ipsum
-  </Tooltip>
-)
-const secondTooltip = (
-  <Tooltip color='#126BCC' message='another tooltip message'>
-    officiis
-  </Tooltip>
-)
+// const firstTooltip = (
+//   <Tooltip color='hotpink' message='tooltip message'>
+//    ipsum
+//   </Tooltip>
+// )
+// const secondTooltip = (
+//   <Tooltip color='#126BCC' message='another tooltip message'>
+//     officiis
+//   </Tooltip>
+// )
 
-function App() {
-  return (
-    <main className='App'>
-      <div className='data-div'>
-      <TheDate />
-      <Counter />
-      </div>
-      <Split className='left' flexBasis='2'>
-        This is the content for the left Split. Lorem {firstTooltip} dolor sit amet consectetur, adipisicing elit. Incidunt ex velit suscipit facere officia?<br />
-        {/* make another tooltip directly inside the App */}
-        <Tooltip message='one more tooltip message'>
-          Necessitatibus?
-        </Tooltip>
-      </Split>
-      <Split className='right'>
-        This is the content for the right Split. Inventore aliquid cupiditate suscipit repellat. Quaerat quis {secondTooltip} quam fuga. Aliquid quo possimus id soluta aspernatur.
-      </Split>
-    </main>
-  )
+const tabsProp = [
+  { name: 'First tab',
+    content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam exercitationem quos consectetur expedita consequatur. Fugit, sapiente aspernatur corporis velit, dolor eum reprehenderit provident ipsam, maiores incidunt repellat! Facilis, neque doloremque.' },
+  { name: 'Second tab',
+    content: 'Laboriosam exercitationem quos consectetur expedita consequatur. Fugit, sapiente aspernatur corporis velit, dolor eum reprehenderit provident ipsam, maiores incidunt repellat! Facilis, neque doloremque. Lorem ipsum dolor sit amet consectetur adipisicing elit.' },
+  { name: 'Third tab',
+    content: 'Fugit, sapiente aspernatur corporis velit, dolor eum reprehenderit provident ipsam, maiores incidunt repellat! Facilis, neque doloremque. Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam exercitationem quos consectetur expedita consequatur.' },
+];
+
+class App extends React.Component {
+  render() {
+    return <div>
+      <Tabs tabs={tabsProp} />
+      <Accordion />
+    </div>
+  }
 }
 
 export default App
--- setupTests.js
+++ setupTests.js
@@ -1,5 +1,27 @@
-// jest-dom adds custom jest matchers for asserting on DOM nodes.
-// allows you to do things like:
-// expect(element).toHaveTextContent(/react/i)
-// learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom';
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import { shallow } from 'enzyme'
+import toJson from 'enzyme-to-json'
+import Tabs from './Tabs'
+
+const tabsProp = [
+    { name: 'First tab',
+      content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam exercitationem quos consectetur expedita consequatur. Fugit, sapiente aspernatur corporis velit, dolor eum reprehenderit provident ipsam, maiores incidunt repellat! Facilis, neque doloremque.' },
+    { name: 'Second tab',
+      content: 'Laboriosam exercitationem quos consectetur expedita consequatur. Fugit, sapiente aspernatur corporis velit, dolor eum reprehenderit provident ipsam, maiores incidunt repellat! Facilis, neque doloremque. Lorem ipsum dolor sit amet consectetur adipisicing elit.' },
+    { name: 'Third tab',
+      content: 'Fugit, sapiente aspernatur corporis velit, dolor eum reprehenderit provident ipsam, maiores incidunt repellat! Facilis, neque doloremque. Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam exercitationem quos consectetur expedita consequatur.' },
+  ];
+
+it('renders empty given no tabs', () => {
+    const wrapper = shallow(<Tabs />)
+    expect(toJson(wrapper)).toMatchSnapshot()
+  })
+
+  it('closes the first tab and opens any clicked tab', () => {
+    const wrapper = shallow(<Tabs tabs={tabsProp} />)
+    wrapper.find('button').at(1).simulate('click')
+    expect(toJson(wrapper)).toMatchSnapshot()
+  })
+
+configure({ adapter: new Adapter() });
